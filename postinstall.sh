2c367314c8fd54e37ea958f9309b018385472813
ESP_IDE <milikiller@seznam.cz>
2025-07-17 21:21:20 +0200

    Fix Linux acces rules on serial devices
    Fix user rights on serial ports

---
+++ postinstall.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "[ESP-IDE] Instalace pravidel pro USB zařízení..."
+RULES_FILE="/etc/udev/rules.d/99-espide-serial.rules"
+
+# Zkopíruj pravidla (musí se spustit pod sudo z .deb balíčku)
+install -m 644 ./99-espide-serial.rules "$RULES_FILE"
+
+# Přidej uživatele do dialout, pokud ještě není
+if ! groups "$USER" | grep -q dialout; then
+  echo "[ESP-IDE] Přidávám uživatele '$USER' do skupiny 'dialout'"
+  usermod -aG dialout "$USER"
+  echo "[ESP-IDE] Projeví se po odhlášení/přihlášení."
+fi
+
+udevadm control --reload-rules
+udevadm trigger
+
+echo "[ESP-IDE] Nastavení sériových portů dokončeno."
